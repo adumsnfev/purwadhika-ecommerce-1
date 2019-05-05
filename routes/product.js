e05b9062957a6f943133d20900d76a80f6cd7776
adum Snfev <maenlagyuk@gmail.com>
2019-05-05 15:52:20 +0700

    added CRUD

--- routes/product.js
+++ routes/product.js
@@ -1,29 +1,70 @@
 const express = require('express')
 const router = express.Router()
-const slugify = require ('slugify')
-const uniqid = require ('uniqid')
 const Product = require('../models/product')
-
-
+const slugify = require('slugify')
+const uniqid = require('uniqid')
+const multer = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './public/img/products');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname);
+  }
+})
+const upload = multer({ storage })
 
 router.get('/', (req, res) => {
-  res.send('halaman produk')
+  Product.find({}, (err, data) => {
+    if (err) {
+      res.send(500)
+    }
+    res.send(data)
+  })
 })
 
-router.post('/', (req, res) => {
-  res.send(req.body)
-
+router.post('/:id', upload.any(), (req, res) => {
   const { name, variants } = req.body
+  const { id } = req.params
+
+    Product.findByIdAndUpdate(id, { name, variants: JSON.parse(variants) }, err => {
+      if (err) {
+        res.send(500)
+      }
+      console.log('data berhasil diupdate ke database')
+      res.send(200)
+    })
+  }
+
+)
+
+router.post('/', upload.any(), (req, res) => {
+  const { name, variants } = req.body
+
 
   new Product({
     name,
     slug: uniqid(`${slugify(name.toLowerCase())}-`),
-    rating: 0,
-    timestamp: new Date(),
     variants: JSON.parse(variants),
-    reviews: []
-  }).save().then(() => {
-    console.log('data berhasil diinput')
+  }).save()
+    .then(() => {
+      console.log('data berhasil disimpan ke database')
+      res.send(200)
+    })
+    .catch(err => {
+      console.log(err)
+      res.send(500)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params
+  Product.findByIdAndDelete(id, err => {
+    if (err) {
+      res.send(500)
+    }
+    console.log('data berhasil dihapus')
+    res.send(200)
   })
 })
 
--- server.js
+++ server.js
@@ -3,17 +3,14 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const bodyParser = require ('body-parser')
-const multer = require ('multer')
+const bodyParser = require('body-parser')
 const productRoutes = require('./routes/product')
 const app = express()
 
 app.use(cors())
-//cors biar bisa beda url, satunya 8000 satunya 3000
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(multer().any())
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 mongoose.connect(
   process.env.DB_CLUSTER_URL,
